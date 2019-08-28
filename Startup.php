a06a66ef610959cfa0056c369cfb94177926a6a0
Rahmat Haryadi <rahmat.haryadi95@gmail.com>
2019-08-28 17:29:08 +0700

    Create Startup.php

---
+++ Startup.php
@@ -0,0 +1,32 @@
+[
+	{
+		"name": "Tokopedia",
+		"description": "BOA",
+		"picture": "A"
+	},
+	{
+		"name": "Bukalapak",
+		"description": "BOA",
+		"picture": "D"
+	},
+	{
+		"name": "Bukalapak",
+		"description": "BOA",
+		"picture": "D"
+	},
+	{
+		"name": "Bukalapak",
+		"description": "BOA",
+		"picture": "D"
+	},
+	{
+		"name": "Bukalapak",
+		"description": "BOA",
+		"picture": "D"
+	},
+	{
+		"name": "Bukalapak",
+		"description": "BOA",
+		"picture": "D"
+	}
+]
